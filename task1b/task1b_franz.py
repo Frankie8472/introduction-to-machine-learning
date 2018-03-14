632062f8553fa78e00f92ce40a5ca8791245c818
Frankie <knobelf@student.ethz.ch>
2018-03-14 17:08:24 +0100

    finished task1b

--- task1b/task1b_franz.py
+++ task1b/task1b_franz.py
@@ -5,8 +5,7 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.utils import shuffle
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
@@ -17,42 +16,21 @@ rmse = []
 mean_rmse = []
 
 # Import data
-data = pd.read_csv("train.csv")
-
-# Shuffle data
-data = shuffle(data, random_state=seed)
+data = pd.read_csv("train.csv", index_col="Id")
 
 # Convert to matrix
 data = data.as_matrix()
 
-# Split into chunks
-data_set = np.array_split(data, indices_or_sections=n)
+# Add functions of x_i
+X = data[:, 1:]
+data = np.c_[data, X**2, np.exp(X), np.cos(X), np.ones(np.alen(X))]
+X = data[:, 1:]
+y = data[:, 0]
 
-
-
-
-# Train
-for alpha in alphas:
-    clf = Ridge(alpha=alpha, copy_X=True, solver="auto")
-
-    for i in range(0, n):
-        y_test = data_set[i][:, 1]
-        X_test = data_set[i][:, 2:]
-
-        train_set = np.concatenate(data_set[i:])
-        y_train = train_set[:, 1]
-        X_train = train_set[:, 2:]
-
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-
-        rmse.append(mean_squared_error(y_test, y_pred) ** 0.5)
-
-    mean_rmse.append(np.mean(rmse))
-    rmse = []
-
+# Linear regression
+clf = LinearRegression()
+clf.fit(X, y)
+w = clf.coef_
 # Print solution to file
-result = pd.DataFrame(mean_rmse)
+result = pd.DataFrame(w)
 result.to_csv("sample_franz.csv", index=False, header=False)
-
-print(mean_rmse)
