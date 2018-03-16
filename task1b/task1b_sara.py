f1c82fcb51b50df92f5efba70e2076afba722dd3
sarstein <sarstein@student.ethz.ch>
2018-03-16 11:48:22 +0100

    first try

--- task1b/task1b_sara.py
+++ task1b/task1b_sara.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 import numpy as np
-
-
+from sklearn.linear_model import RidgeCV
 
 data = pd.read_csv("train.csv")
 data = data.as_matrix()
+print(type(data))
 
 n = np.shape(data)[0]
 
@@ -14,6 +14,17 @@ X_total = data[0:, 2:]
 Y_total = data[0:, 1]
 
 X_total = np.c_[X_total, X_total**2, np.exp(X_total), np.cos(X_total), np.ones(np.alen(X_total))]
+
+
+clf = RidgeCV(alphas=[1], cv=10, fit_intercept=False)  # tol = 0.0001
+
+clf.fit(X_total, Y_total, None)
+a = clf.coef_
+
+result = pd.DataFrame(a)
+result.to_csv("task1b_sara.csv", index=False, header=False)
+
+
 
 #find correct lamda to minimize the error
 
