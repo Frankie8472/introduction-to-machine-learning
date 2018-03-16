4ad96aead7c6a50dc10c277b45267bf643b96b05
sarstein <sarstein@student.ethz.ch>
2018-03-16 15:19:15 +0100

    rigedcv with both versions equivalent

--- task1b/task1b_sara.py
+++ task1b/task1b_sara.py
@@ -9,12 +9,12 @@ data = hallo.as_matrix()
 print(type(data))
 
 n = np.shape(data)[0]
-l=[0.1,1,10,100,100,1000, 10000]
+l=[0.01, 0.1,1,10,100,100,1000, 10000]
 
 
 X_total = data[:, 1:]
 Y_total = data[:, 0]
-#print ((pd.read_csv("train.csv")['y'])==Y_total) #make sure it is the same y nicolas uses
+
 
 
 ########################333 nicu
@@ -30,13 +30,14 @@ X_teq = pd.concat([X_teq,
                     X_teq.applymap(lambda x: np.cos(x))],
                     axis=1)
 X_teq['x21'] = 1
-
 ############################3
 
 
 
 
 X_total = np.c_[X_total, X_total**2, np.exp(X_total), np.cos(X_total), np.ones(np.alen(X_total))]
+
+
 
 
 ###########3333 nicu
