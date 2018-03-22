671fa0b7b18d3c39f05b18d23cb61e001d1b8f26
Frankie <knobelf@student.ethz.ch>
2018-03-22 19:59:39 +0100

    wrote 1b anew

--- task1a/task1a_franz.py
+++ task1a/task1a_franz.py
@@ -5,9 +5,11 @@
 
 import pandas as pd
 import numpy as np
+from pandas import read_csv, DataFrame
+from numpy import mean
 from sklearn.model_selection import KFold
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, make_scorer
 
 # Parameter initialization
 alphas = [0.1, 1.0, 10.0, 100.0, 1000.0]
@@ -15,9 +17,32 @@ n = 10
 rmse = []
 mean_rmse = []
 
+
+# Define exercise functions
+def read_csv_to_matrix(filename, index_name):
+    return read_csv(filename, index_col=index_name).as_matrix()
+
+
+def write_to_csv_from_vector(filename, vec):
+    return DataFrame(vec).to_csv(filename, index=False, header=False)
+
+
+def split_into_x_y(data_set):
+    y = data_set[:, 0]
+    x = data_set[:, 1:]
+    return x, y
+
+
+def root_mean_squared_error(y, y_pred):
+    return mean_squared_error(y, y_pred) ** 0.5
+
+
+# Scorer
+loss_func = make_scorer(root_mean_squared_error, greater_is_better=False)
+
+
 # Import data and split
-data = pd.read_csv("train.csv", index_col="Id")
-data = data.as_matrix()
+data = read_csv_to_matrix("train.csv", "Id")
 kf = KFold(n_splits=n, shuffle=False, random_state=None)
 
 # Train
@@ -31,13 +56,10 @@ for alpha in alphas:
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
 
-        rmse.append(mean_squared_error(y_test, y_pred) ** 0.5)
+        rmse.append(root_mean_squared_error(y_test, y_pred))
 
-    mean_rmse.append(np.mean(rmse))
+    mean_rmse.append(mean(rmse))
     rmse = []
 
 # Print solution to file
-result = pd.DataFrame(mean_rmse)
-result.to_csv("sample_franz.csv", index=False, header=False)
-
-print(mean_rmse)
+write_to_csv_from_vector("sample_franz.csv", mean_rmse)
--- task1b/task1b_franz.py
+++ task1b/task1b_franz.py
@@ -1,71 +1,88 @@
-
 # train.csv     - the training set
 # test.csv      - the test set (make predictions based on this file)
 # sample.csv    - a sample submission file in the correct format
 
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
+from pandas import read_csv
+from pandas import DataFrame
+from sklearn.linear_model import RidgeCV, Ridge
+from sklearn.metrics import make_scorer, mean_squared_error
+
+# Define parameter specs
+alphas = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
-alphas = np.linspace(90.0, 91.0, 10000*(10**0))
-n = 10
-tol = 0.0001
-rmse = []
-mean_rmse = []
-best = (0, 0)
-
-# Import data
-data = pd.read_csv("train.csv", index_col="Id")
-
-# Convert to matrix
-data = data.as_matrix()
-
-# Add functions of x_i
-y = data[:, 0]
-X = data[:, 1:]
-X = np.c_[X, X**2, np.exp(X), np.cos(X), np.ones(np.alen(X))]
-
-# Split into Folds
-kf = KFold(n_splits=n, shuffle=False, random_state=None)
-
-# Train
-for alpha in alphas:
-    clf = Ridge(alpha=alpha,
-                fit_intercept=False,
-                normalize=False,
-                copy_X=True,
-                max_iter=1000,
-                tol=tol,
-                solver="auto",
-                random_state=None
-                )
-
-    for train_index, test_index in kf.split(data):
-        y_train, y_test = data[train_index, 0], data[test_index, 0]
-        X_train, X_test = data[train_index, 1:], data[test_index, 1:]
-
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-
-        rmse.append(mean_squared_error(y_test, y_pred) ** 0.5)
-    print(str(alpha) + ", " + str(np.mean(rmse)))
-    mean_rmse.append(np.mean(rmse))
-    rmse = []
+# Define exercise functions
+def read_csv_to_matrix(filename, index_name):
+    return read_csv(filename, index_col=index_name).as_matrix()
 
 
-# Print solution to file
-#result = pd.DataFrame(mean_rmse)
-#result.to_csv("sample_franz.csv", index=False, header=False)
-for i, j in enumerate(mean_rmse):
-    if j == min(mean_rmse):
-        print("alpha = " + str(alphas[i]))
+def write_to_csv_from_vector(filename, vec):
+    return DataFrame(vec).to_csv(filename, index=False, header=False)
 
 
+def split_into_x_y(data_set):
+    y = data_set[:, 0]
+    x = data_set[:, 1:]
+    return x, y
 
-# Print solution to file
-#result = pd.DataFrame(weights)
-#result.to_csv("sample_franz.csv", index=False, header=False)
-#print(weights)
+
+def root_mean_squared_error(y, y_pred):
+    return mean_squared_error(y, y_pred) ** 0.5
+
+
+def square_element_wise(matrix):
+    return matrix ** 2
+
+
+def exp_element_wise(matrix):
+    return np.exp(matrix)
+
+
+def cos_element_wise(matrix):
+    return np.cos(matrix)
+
+
+def ones_vector_same_length(matrix):
+    return np.ones(np.alen(matrix))
+
+
+def extend_features(features):
+    return np.c_[features,
+                 square_element_wise(features),
+                 exp_element_wise(features),
+                 cos_element_wise(features),
+                 ones_vector_same_length(features)]
+
+
+# Get, split and transform dataset
+data = read_csv_to_matrix("train.csv", "Id")
+X_train, y_train = split_into_x_y(data)
+X_train = extend_features(X_train)
+
+# Loss function
+loss_func = make_scorer(root_mean_squared_error, greater_is_better=False)
+
+# Regressor
+reg = RidgeCV(alphas=alphas,
+              fit_intercept=True,
+              scoring=loss_func,
+              cv=None)
+
+# Train with cv
+reg.fit(X_train, y_train)
+
+# Get ideal parameters
+best_alpha = reg.alpha_
+print(best_alpha)
+
+clf = Ridge(alpha=best_alpha,
+            fit_intercept=True,
+            )
+clf.fit(X_train, y_train)
+
+# Calculate weights with the ideal parameters
+weights = clf.coef_
+
+# Write result in file
+write_to_csv_from_vector("sample_franz.csv", weights)
