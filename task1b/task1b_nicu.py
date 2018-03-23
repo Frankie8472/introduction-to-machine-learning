d225c5f4bb785c335e9229434cef5925ed02dbbc
NicoPicoLino <n.e.ruegsegger@gmail.com>
2018-03-23 10:44:47 +0100

    Committing final attempt

--- task1b/task1b_nicu.py
+++ task1b/task1b_nicu.py
@@ -10,10 +10,10 @@ from sklearn.linear_model import RidgeCV
 # Import CSV as pandas dataframe
 df = pd.read_csv('train.csv', index_col='Id')
 
-# Define alphas and k
-alphas = [0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
-#k = 10
-dataframe
+# Define alphas
+#alphas = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+alphas = [1000.0]
+
 # Apply feature transformation and set up data for training
 X_plain = df.iloc[:,1:]
 X_train = pd.concat([X_plain,
@@ -25,7 +25,7 @@ X_train['x21'] = 1
 y_train = df['y']
 
 # Perform training with ridge regression and cross-validation
-reg = RidgeCV(alphas=alphas, fit_intercept=True, normalize=True)
+reg = RidgeCV(alphas=alphas, fit_intercept=False, normalize=False)
 reg.fit(X_train,y_train)
 
 # Get weights
@@ -34,3 +34,4 @@ weights = reg.coef_
 # Print solution to file
 result = pd.DataFrame(weights)
 result.to_csv("output_nicu.csv", index=False, header=False)
+print(reg.alpha_)
