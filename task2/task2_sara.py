3167186b99ab655f9589fc75d2349a63c6ddb552
sarstein <sarstein@student.ethz.ch>
2018-03-27 18:14:29 +0200

    with SVC

--- task2/task2_sara.py
+++ task2/task2_sara.py
@@ -10,6 +10,7 @@ index = index[:, 0]
 train = pd.read_csv("train.csv", index_col="Id")
 train = train.as_matrix()
 
+
 test = pd.read_csv("test.csv", index_col="Id")
 test = test.as_matrix()
 
@@ -18,20 +19,23 @@ y_train = train[:, 0]
 
 x_test = test
 
-logistic = LogisticRegressionCV(Cs=[1e5])
+logistic = LogisticRegressionCV(Cs=[0.0000001, 0.00001, 0.0001, 0.001, 0.01, 0.1], cv=60, fit_intercept=True, penalty="l2")
 logistic.fit(x_train, y_train)
 y_pred = logistic.predict(x_test)
+print("logistics CS :\n",logistic.Cs)
+print("logistics CS_:\n",logistic.Cs_)
+print("logistics C_:\n",logistic.C_)
 
-print(y_pred[0:10],type(y_pred))
-print(index[0:10], type(index))
+#print(y_pred[0:10],type(y_pred))
+#print(index[0:10], type(index))
 
 #pach y pred to index
 result = np.c_[index, y_pred]
-print(type(result))
-print(result[:, :])
+#print(type(result))
+#print(result[:, :])
 #;alskfdj
 
 
 
 output = pd.DataFrame(result)
-output.to_csv("task2_sara.csv", index=False, header=["Id","y"])
+output.to_csv("task2_sara.csv", index=False, header=["Id", "y"])
