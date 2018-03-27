6a0fb94d3fdedd708d0491b0e059c5eac7b146a3
sarstein <sarstein@student.ethz.ch>
2018-03-27 16:45:09 +0200

    first try with logistic model

--- task2/task2_sara.py
+++ task2/task2_sara.py
@@ -2,24 +2,36 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegressionCV
 
-index = pd.read_csv()
-index = pd.as_matrix(index)[0,:]
+index = pd.read_csv("test.csv")
+index = index.as_matrix()
+print(type(index))
+index = index[:, 0]
 
-train = pd.read_csv("train.csv", Index=False)
-train = pd.as_matrix(train)
+train = pd.read_csv("train.csv", index_col="Id")
+train = train.as_matrix()
 
-test = pd.read_csv("test.csv", Index=False)
-test = pd.as_matrix(test)
+test = pd.read_csv("test.csv", index_col="Id")
+test = test.as_matrix()
 
-x_train = train[1:, :]
-y_train = train[0, :]
+x_train = train[:, 1:]
+y_train = train[:, 0]
 
 x_test = test
 
-logistic = LogisticRegressionCV(C = e5)
+logistic = LogisticRegressionCV(Cs=[1e5])
 logistic.fit(x_train, y_train)
 y_pred = logistic.predict(x_test)
 
-result =
-result [0,:] = index
-result [1,:] = y_pred
+print(y_pred[0:10],type(y_pred))
+print(index[0:10], type(index))
+
+#pach y pred to index
+result = np.c_[index, y_pred]
+print(type(result))
+print(result[:, :])
+#;alskfdj
+
+
+
+output = pd.DataFrame(result)
+output.to_csv("task2_sara.csv", index=False, header=False)
