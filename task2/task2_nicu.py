9ee5468a353a12c39072ee41a2d2605a4f6d2fa5
NicoPicoLino <n.e.ruegsegger@gmail.com>
2018-04-09 08:55:07 +0200

    Use MLPClassifier with alpha=10 yielding a public score of 0.8969

--- task2/task2_nicu.py
+++ task2/task2_nicu.py
@@ -5,23 +5,23 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
 
 # Import CSV as pandas dataframe
 df = pd.read_csv('train.csv', index_col='Id')
 test = pd.read_csv('test.csv', index_col='Id')
 
-# Splitt up data for training
+# Split up data for training
 X_train = df.iloc[:,1:]
 y_train = df['y']
 
-# Perform training with one-vs-one multiclass strategy
-ovoc = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X_train, y_train)
+# Perform training with Multi-layer Perceptron classifier
+clf = MLPClassifier(activation='tanh', alpha=10, max_iter=10000, solver='lbfgs')
+clf.fit(X_train, y_train)
 
 # Get results
-y_out = ovoc.predict(test)
+y_out = clf.predict(test)
 
 # Print solution to file
 result = pd.DataFrame(data=y_out, index=test.index.values)
-result.to_csv("./output/output_nicu.csv", index=True, index_label="Id", header=['y'])
+result.to_csv("./output/output_nicu_neuralnet3.csv", index=True, index_label="Id", header=['y'])
